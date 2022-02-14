d7813174b4ee9e251961a5e503669b6992ed2fb4
Henry Addison <henryaddison@users.noreply.github.com>
2022-02-14 12:04:13 +0000

    further align dataset metadata between ceda and moose versions

--- src/ml_downscaling_emulator/bin/moose.py
+++ src/ml_downscaling_emulator/bin/moose.py
@@ -28,65 +28,65 @@ class SubDomainOption(str, Enum):
 def callback():
     pass
 
-def variable_dirpath(variable: str, year: int, temporal_res: str):
-    return Path(os.getenv("MOOSE_DATA"))/f"{temporal_res}_{variable}_{year}"
+def variable_dirpath(variable: str, year: int, frequency: str):
+    return Path(os.getenv("MOOSE_DATA"))/f"{frequency}_{variable}_{year}"
 
-def data_dirpath(variable: str, year: int, temporal_res: str):
-    return variable_dirpath(variable=variable, year=year, temporal_res=temporal_res)/"data"
+def data_dirpath(variable: str, year: int, frequency: str):
+    return variable_dirpath(variable=variable, year=year, frequency=frequency)/"data"
 
-def ppdata_dirpath(variable: str, year: int, temporal_res: str):
-    return data_dirpath(variable=variable, year=year, temporal_res=temporal_res)/"pp"
+def ppdata_dirpath(variable: str, year: int, frequency: str):
+    return data_dirpath(variable=variable, year=year, frequency=frequency)/"pp"
 
-def nc_filename(variable: str, year: int, temporal_res: str, domain: str = "uk"):
-    return f"{variable}_rcp85_land-cpm_{domain}_2.2km_01_{temporal_res}_{year-1}1201-{year}1130.nc"
+def nc_filename(variable: str, year: int, frequency: str, domain: str = "uk", resolution: str = "2.2km"):
+    return f"{variable}_rcp85_land-cpm_{domain}_2.2km_01_{frequency}_{year-1}1201-{year}1130.nc"
 
-def raw_nc_filepath(variable: str, year: int, temporal_res: str):
-    return data_dirpath(variable=variable, year=year, temporal_res=temporal_res)/nc_filename(variable=variable, year=year, temporal_res=temporal_res)
+def raw_nc_filepath(variable: str, year: int, frequency: str):
+    return data_dirpath(variable=variable, year=year, frequency=frequency)/nc_filename(variable=variable, year=year, frequency=frequency)
 
-def processed_nc_filepath(variable: str, year: int, temporal_res: str, domain: str = "uk"):
-    return Path(os.getenv("DERIVED_DATA"))/"moose"/variable/nc_filename(variable=variable, year=year, temporal_res=temporal_res, domain=domain)
+def processed_nc_filepath(variable: str, year: int, frequency: str, domain: str = "uk", resolution: str = "2.2km"):
+    return Path(os.getenv("DERIVED_DATA"))/"moose"/domain/resolution/"rcp85"/"01"/variable/frequency/nc_filename(variable=variable, year=year, frequency=frequency, domain=domain, resolution=resolution)
 
 @app.command()
-def extract(variable: str = typer.Option(...), year: int = typer.Option(...), temporal_res: str = "day"):
+def extract(variable: str = typer.Option(...), year: int = typer.Option(...), frequency: str = "day"):
     """
     Extract data from moose
     """
-    query = select_query(year=year, variable=variable, temporal_res=temporal_res)
+    query = select_query(year=year, variable=variable, frequency=frequency)
 
-    output_dirpath = Path(os.getenv("MOOSE_DATA"))/f"{temporal_res}_{variable}_{year}"
-    query_filepath = variable_dirpath(variable=variable, year=year, temporal_res=temporal_res)/"searchfile"
+    output_dirpath = Path(os.getenv("MOOSE_DATA"))/f"{frequency}_{variable}_{year}"
+    query_filepath = variable_dirpath(variable=variable, year=year, frequency=frequency)/"searchfile"
 
     os.makedirs(output_dirpath, exist_ok=True)
-    os.makedirs(ppdata_dirpath(variable=variable, year=year, temporal_res=temporal_res), exist_ok=True)
+    os.makedirs(ppdata_dirpath(variable=variable, year=year, frequency=frequency), exist_ok=True)
 
     typer.echo(query)
     query_filepath.write_text(query)
 
-    moose_uri = moose_path(variable, year, ensemble_member=1, temporal_res="day")
+    moose_uri = moose_path(variable, year, ensemble_member=1, frequency="day")
 
-    query_cmd = ["moo" , "select", query_filepath, moose_uri, os.path.join(ppdata_dirpath(variable=variable, year=year, temporal_res=temporal_res),"")]
+    query_cmd = ["moo" , "select", query_filepath, moose_uri, os.path.join(ppdata_dirpath(variable=variable, year=year, frequency=frequency),"")]
 
     typer.echo(f"Running {query_cmd}")
     os.execvp(query_cmd[0], query_cmd)
 
 @app.command()
-def convert(variable: str = typer.Option(...), year: int = typer.Option(...), temporal_res: str = "day"):
+def convert(variable: str = typer.Option(...), year: int = typer.Option(...), frequency: str = "day"):
     """
     Convert pp data to a netCDF file
     """
-    pp_files_glob = ppdata_dirpath(variable=variable, year=year, temporal_res=temporal_res)/"*.pp"
-    output_filepath = raw_nc_filepath(variable=variable, year=year, temporal_res=temporal_res)
+    pp_files_glob = ppdata_dirpath(variable=variable, year=year, frequency=frequency)/"*.pp"
+    output_filepath = raw_nc_filepath(variable=variable, year=year, frequency=frequency)
 
     typer.echo(f"Saving to {output_filepath}...")
     iris.save(iris.load(str(pp_files_glob)), output_filepath)
 
 @app.command()
-def preprocess(variable: str = typer.Option(...), year: int = typer.Option(...), temporal_res: str = "day", scale_factor: int = typer.Option(...), subdomain: SubDomainOption = SubDomainOption.london):
+def preprocess(variable: str = typer.Option(...), year: int = typer.Option(...), frequency: str = "day", scale_factor: int = typer.Option(...), subdomain: SubDomainOption = SubDomainOption.london):
     """
     Coarsen data by given scale-factor
     """
-    input_filepath = raw_nc_filepath(variable=variable, year=year, temporal_res=temporal_res)
-    output_filepath = processed_nc_filepath(variable=variable, year=year, temporal_res=temporal_res, domain=subdomain)
+    input_filepath = raw_nc_filepath(variable=variable, year=year, frequency=frequency)
+    output_filepath = processed_nc_filepath(variable=variable, year=year, frequency=frequency, domain=subdomain, resolution=f"2.2km-coarsened-{scale_factor}x")
     ds = xr.load_dataset(input_filepath)
 
     if subdomain == SubDomainOption.london:
@@ -102,11 +102,11 @@ def preprocess(variable: str = typer.Option(...), year: int = typer.Option(...),
     ds.to_netcdf(output_filepath)
 
 @app.command()
-def clean(variable: str = typer.Option(...), year: int = typer.Option(...), temporal_res: str = "day"):
+def clean(variable: str = typer.Option(...), year: int = typer.Option(...), frequency: str = "day"):
     """
     Remove any unneccessary files once conversion is done
     """
-    typer.echo(f"Removing {ppdata_dirpath(variable=variable, year=year, temporal_res=temporal_res)}...")
-    shutil.rmtree(ppdata_dirpath(variable=variable, year=year, temporal_res=temporal_res))
-    typer.echo(f"Removing {raw_nc_filepath(variable=variable, year=year, temporal_res=temporal_res)}...")
-    os.remove(raw_nc_filepath(variable=variable, year=year, temporal_res=temporal_res))
+    typer.echo(f"Removing {ppdata_dirpath(variable=variable, year=year, frequency=frequency)}...")
+    shutil.rmtree(ppdata_dirpath(variable=variable, year=year, frequency=frequency))
+    typer.echo(f"Removing {raw_nc_filepath(variable=variable, year=year, frequency=frequency)}...")
+    os.remove(raw_nc_filepath(variable=variable, year=year, frequency=frequency))
