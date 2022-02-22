55def61a8d7f021ec9e6d068ee6b4883852537a2
Henry Addison <henryaddison@users.noreply.github.com>
2022-02-22 10:27:27 +0000

    update ssi split cli
    move into typer cli app
    output an extreme and normal test and val sets

--- src/ml_downscaling_emulator/bin/__init__.py
+++ src/ml_downscaling_emulator/bin/__init__.py
@@ -1,9 +1,12 @@
 import typer
 
 from ml_downscaling_emulator.bin import moose
+from ml_downscaling_emulator.bin import dataset
+
 
 app = typer.Typer()
 app.add_typer(moose.app, name="moose")
+app.add_typer(dataset.app, name="dataset")
 
 if __name__ == "__main__":
     app()
