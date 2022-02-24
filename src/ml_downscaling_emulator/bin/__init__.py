39dba7041b5c3b630c3a8ee47387fdbe03ccf00d
Henry Addison <henryaddison@users.noreply.github.com>
2022-02-24 14:00:57 +0000

    incorporate old preprocessing in to mlde cli under ceda
    to mirror the new moose functionality

--- src/ml_downscaling_emulator/bin/__init__.py
+++ src/ml_downscaling_emulator/bin/__init__.py
@@ -1,12 +1,15 @@
 import typer
 
-from ml_downscaling_emulator.bin import moose
-from ml_downscaling_emulator.bin import dataset
 
+from ml_downscaling_emulator.bin import ceda
+from ml_downscaling_emulator.bin import dataset
+from ml_downscaling_emulator.bin import moose
 
 app = typer.Typer()
-app.add_typer(moose.app, name="moose")
+app.add_typer(ceda.app, name="ceda")
 app.add_typer(dataset.app, name="dataset")
+app.add_typer(moose.app, name="moose")
+
 
 if __name__ == "__main__":
     app()
