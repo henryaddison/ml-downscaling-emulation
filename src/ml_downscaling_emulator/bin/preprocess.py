e289eb8f47dafc4b77d130192a5f4198ea710a5f
Henry Addison <henryaddison@users.noreply.github.com>
2022-02-28 14:07:56 +0000

    add a create variable cli action

---
+++ src/ml_downscaling_emulator/bin/preprocess.py
@@ -0,0 +1,55 @@
+from importlib_resources import files
+import logging
+import os
+from typing import List
+import yaml
+
+import typer
+import xarray as xr
+
+from ml_downscaling_emulator import UKCPDatasetMetadata
+from ml_downscaling_emulator.bin import DomainOption
+from ml_downscaling_emulator.preprocessing.sum import Sum
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO, format='%(levelname)s %(asctime)s: %(message)s')
+
+app = typer.Typer()
+
+@app.command()
+def create_variable(variable: str = typer.Option(...), year: int = typer.Option(...), resolution: str = typer.Option(...), frequency: str = "day", domain: DomainOption = DomainOption.london, scenario="rcp85"):
+    """
+    Create a new variable
+    """
+    config = files('ml_downscaling_emulator.config').joinpath(f'variables/day/{variable}.yml').read_text()
+    config = yaml.safe_load(config)
+
+    data_basedir = os.path.join(os.getenv("DERIVED_DATA"), "moose")
+
+    output_metadata = UKCPDatasetMetadata(data_basedir, frequency=frequency, domain=domain, resolution=resolution, ensemble_member='01', variable=config['variable'])
+
+    for job_spec in config['spec']:
+        if job_spec['action'] == "sum":
+            logger.info(f"Summing {job_spec['variables']}")
+            input_metadata = [
+                UKCPDatasetMetadata(data_basedir, frequency=frequency, domain=domain, resolution=resolution, ensemble_member='01', variable=variable) for variable in job_spec['variables']
+            ]
+
+            ds = xr.open_mfdataset([m.filepath(year) for m in input_metadata])
+            ds = Sum([m.variable for m in input_metadata], output_metadata.variable).run(ds)
+            ds = ds[variable].assign_attrs(config['attrs'])
+
+    logger.info(f"Saving data to {output_metadata.filepath(year)}")
+    os.makedirs(output_metadata.dirpath(), exist_ok=True)
+    ds.to_netcdf(output_metadata.filepath(year))
+
+
+# @app.command()
+# def sum(variables: List[str] = typer.Option(...), output_variable: str = typer.Option(...), year: int = typer.Option(...), resolution: str = typer.Option(...), frequency: str = "day", domain: DomainOption = DomainOption.london):
+#     metadata = [
+#         UKCPDatasetMetadata(frequency=frequency, domain=domain, resolution=resolution, ensemble_member='01', variable=variable) for variable in variables
+#     ]
+#     filepaths = [m.filepath(year) for m in metadata]
+#     output_metadata = UKCPDatasetMetadata(frequency=frequency, domain=domain, resolution=resolution, ensemble_member='01', variable=output_variable)
+#     xr.open_mfdataset(filepaths)
+#     typer.echo(f"Summing {variables}")
