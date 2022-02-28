c14f9c72f1b606beaf7751f25435e4fab9beaccf
Henry Addison <henryaddison@users.noreply.github.com>
2022-02-28 14:07:56 +0000

    add a create variable cli action

--- src/ml_downscaling_emulator/bin/__init__.py
+++ src/ml_downscaling_emulator/bin/__init__.py
@@ -8,12 +8,13 @@ class DomainOption(str, Enum):
 from ml_downscaling_emulator.bin import ceda
 from ml_downscaling_emulator.bin import dataset
 from ml_downscaling_emulator.bin import moose
+from ml_downscaling_emulator.bin import preprocess
 
 app = typer.Typer()
 app.add_typer(ceda.app, name="ceda")
 app.add_typer(dataset.app, name="dataset")
 app.add_typer(moose.app, name="moose")
-
+app.add_typer(preprocess.app, name="preprocess")
 
 if __name__ == "__main__":
     app()
