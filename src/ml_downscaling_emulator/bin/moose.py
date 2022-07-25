9e8670688b9c3ad2608717ecad7d17f429d65f19
Henry Addison <henryaddison@users.noreply.github.com>
2022-07-25 15:44:14 +0100

    remove forecast metadata from target grid files
    really don't need it here, only need these files for regridding

--- src/ml_downscaling_emulator/bin/moose.py
+++ src/ml_downscaling_emulator/bin/moose.py
@@ -278,7 +278,7 @@ def create_variable(variable: str = typer.Option(...), year: int = typer.Option(
         elif job_spec['action'] == "regrid":
             if target_resolution != variable_resolution:
                 typer.echo(f"Regridding to target resolution...")
-                target_grid_filepath = os.path.join(os.path.dirname(__file__), '..', 'utils', 'target-grids', target_resolution, 'uk', 'moose_pr_grid.nc')
+                target_grid_filepath = os.path.join(os.path.dirname(__file__), '..', 'utils', 'target-grids', target_resolution, 'uk', 'moose_grid.nc')
 
                 ds = Regrid(target_grid_filepath, variables=[config['variable']]).run(ds)
         elif job_spec['action'] == "vorticity":
