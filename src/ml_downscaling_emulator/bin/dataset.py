f2836f432538547efc4d16dfabf524d0d061ccdd
Henry Addison <henryaddison@users.noreply.github.com>
2022-11-21 15:40:21 +0000

    absorb check dataset script into mlde dataset subcommand

--- src/ml_downscaling_emulator/bin/dataset.py
+++ src/ml_downscaling_emulator/bin/dataset.py
@@ -2,6 +2,7 @@ from enum import Enum
 import logging
 import os
 from pathlib import Path
+import re
 import yaml
 
 import typer
@@ -67,3 +68,39 @@ def create(config: Path, input_base_dir: Path = typer.Argument(..., envvar="MOOS
         yaml.dump(config, f)
     for split_name, split_ds in split_sets.items():
         split_ds.to_netcdf(os.path.join(output_dir, f"{split_name}.nc"))
+
+
+@app.command()
+def validate():
+    datasets = [
+        "2.2km-coarsened-8x_london_vorticity850_random",
+        "2.2km-coarsened-gcm-2.2km-coarsened-4x_birmingham_vorticity850_random",
+        "60km-2.2km-coarsened-4x_birmingham_vorticity850_random",
+        "2.2km-coarsened-gcm-2.2km_london_vorticity850_random",
+        "60km-2.2km_london_vorticity850_random"
+    ]
+
+    splits = ["train", "val", "test"]
+
+    for dataset in datasets:
+        bad_splits = {"no file": set(), "forecast_encoding": set(), "forecast_vars": set()}
+        for split in splits:
+            print(f"Checking {split} of {dataset}")
+            dataset_path = os.path.join(os.getenv("MOOSE_DERIVED_DATA"), "nc-datasets", dataset, f"{split}.nc")
+            try:
+                ds = xr.open_dataset(dataset_path)
+            except FileNotFoundError:
+                bad_splits["no file"].add(split)
+                continue
+
+            # check for forecast related metadata (should have been stripped)
+            for v in ds.variables:
+                if "coordinates" in ds[v].encoding and (re.match("(realization|forecast_period|forecast_reference_time) ?", ds[v].encoding["coordinates"]) is not None):
+                    bad_splits["forecast_encoding"].add(split)
+                if v in ["forecast_period", "forecast_reference_time", "realization", "forecast_period_bnds"]:
+                    bad_splits["forecast_vars"].add(split)
+
+        # report findings
+        for reason, error_splits in bad_splits.items():
+            if len(error_splits) > 0:
+                print(f"Failed '{reason}': {dataset} for {error_splits}")
