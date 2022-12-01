1a9c6546bbb31cb60d1bb64950aef0e00ca3b5b8
Henry Addison <henryaddison@users.noreply.github.com>
2022-12-01 16:08:56 +0000

    add a way to predict lo-res pr input as output

--- src/ml_downscaling_emulator/bin/evaluation.py
+++ src/ml_downscaling_emulator/bin/evaluation.py
@@ -6,11 +6,12 @@ from knockknock import slack_sender
 import shortuuid
 import torch
 import typer
+import xarray as xr
 import yaml
 
-from ml_downscaling_emulator.evaluation import predict
+from ml_downscaling_emulator.evaluation import predict, samples_to_xr
 from ml_downscaling_emulator.training import restore_checkpoint
-from ml_downscaling_emulator.training.dataset import get_dataset
+from ml_downscaling_emulator.training.dataset import get_dataset, load_raw_dataset_split
 from ml_downscaling_emulator.unet import unet
 
 logging.basicConfig(level=logging.INFO, format='%(levelname)s - %(filename)s - %(asctime)s - %(message)s')
@@ -61,6 +62,31 @@ def sample(
     for sample_id in range(num_samples):
         typer.echo(f"Sample run {sample_id}...")
         predictions = predict(state["model"], eval_dl, target_transform)
+
+        output_filepath = os.path.join(output_dirpath, f"predictions-{shortuuid.uuid()}.nc")
+
+        logger.info(f"Saving predictions to {output_filepath}")
+        os.makedirs(output_dirpath, exist_ok=True)
+        predictions.to_netcdf(output_filepath)
+
+@app.command()
+@Timer(name="sample", text="{name}: {minutes:.1f} minutes", logger=logging.info)
+@slack_sender(webhook_url=os.getenv("KK_SLACK_WH_URL"), channel="general")
+def sample_id(
+        workdir: Path,
+        dataset: str = typer.Option(...),
+        split:str = "val",
+        num_samples: int = 1,
+    ):
+
+    output_dirpath = workdir/"samples"/"epoch-0"/dataset/split
+    os.makedirs(output_dirpath, exist_ok=True)
+
+    for sample_id in range(num_samples):
+        typer.echo(f"Sample run {sample_id}...")
+        eval_ds = load_raw_dataset_split(dataset, split)
+        samples = eval_ds["pr"].values
+        predictions = samples_to_xr(samples, eval_ds, target_transform=None)
 
         output_filepath = os.path.join(output_dirpath, f"predictions-{shortuuid.uuid()}.nc")
 
