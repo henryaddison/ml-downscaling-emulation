ce2f8153ce394875a0f5c8e9ed329c4c6e341c02
Henry Addison <henryaddison@users.noreply.github.com>
2022-12-06 15:41:13 +0000

    helper for training id model

---
+++ bin/train-id-model.py
@@ -0,0 +1,50 @@
+import logging
+import os
+from pathlib import Path
+
+from codetiming import Timer
+from knockknock import slack_sender
+import numpy as np
+import torch
+from tqdm import tqdm
+from tqdm.contrib.logging import logging_redirect_tqdm
+import typer
+import yaml
+
+from ml_downscaling_emulator.unet import unet
+from ml_downscaling_emulator.training import log_epoch, track_run, save_checkpoint
+from ml_downscaling_emulator.training.dataset import get_dataset
+
+UNET_ARCHNAME = "u-net"
+SIMPLE_CONV_ARCHNAME = "simple-conv"
+EXPERIMENT_NAME=os.getenv("WANDB_EXPERIMENT_NAME")
+TAGS = {
+    UNET_ARCHNAME: ["baseline", UNET_ARCHNAME],
+    SIMPLE_CONV_ARCHNAME: ["baseline", SIMPLE_CONV_ARCHNAME, "debug"]
+}
+
+app = typer.Typer()
+
+logging.basicConfig(level=logging.INFO, format='%(levelname)s - %(filename)s - %(asctime)s - %(message)s')
+logger = logging.getLogger()
+logger.setLevel('INFO')
+
+@app.command()
+@Timer(name="train", text="{name}: {minutes:.1f} minutes", logger=logging.info)
+@slack_sender(webhook_url=os.getenv("KK_SLACK_WH_URL"), channel="general")
+def main(
+        workdir: Path,
+    ):
+
+    os.makedirs(workdir, exist_ok=True)
+
+    # Create directories for experimental logs
+    sample_dir = os.path.join(workdir, "samples")
+    os.makedirs(sample_dir, exist_ok=True)
+
+    logging.info(f"Starting {os.path.basename(__file__)}")
+
+    logging.info(f"Finished {os.path.basename(__file__)}")
+
+if __name__ == "__main__":
+    app()
