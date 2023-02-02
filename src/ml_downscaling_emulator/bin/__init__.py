f3c7c2e5ef360b2326a29f2ff4cf690f5e0150da
Henry Addison <henryaddison@users.noreply.github.com>
2023-02-02 16:30:22 +0000

    couple of items missed in mlde_data code transfer

--- src/ml_downscaling_emulator/bin/__init__.py
+++ src/ml_downscaling_emulator/bin/__init__.py
@@ -1,28 +1,9 @@
-from pathlib import Path
-from typing import List
-
 import typer
-import xarray as xr
 
 from . import evaluate
 
 app = typer.Typer()
 app.add_typer(evaluate.app, name="evaluate")
-
-
-@app.command()
-def sample(files: List[Path]):
-    for file in files:
-        ds = xr.open_dataset(file)
-        # take something from each season and each decade
-        sampled_ds = ds.sel(
-            time=((ds["time.month"] % 3 == 0) & (ds["time.year"] % 10 == 0))
-        ).load()
-        ds.close()
-        del ds
-        print(f"Saving {file}")
-        sampled_ds.to_netcdf(file)
-        del sampled_ds
 
 
 if __name__ == "__main__":
