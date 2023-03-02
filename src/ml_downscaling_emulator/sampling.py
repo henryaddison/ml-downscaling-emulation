54a850986454e500a372c66a5a629f17e1e15ef2
Henry Addison <henryaddison@users.noreply.github.com>
2023-03-02 15:18:26 +0000

    update unet sampling to not used shuffled Dataloader

--- src/ml_downscaling_emulator/sampling.py
+++ src/ml_downscaling_emulator/sampling.py
@@ -4,6 +4,8 @@ from tqdm import tqdm
 from tqdm.contrib.logging import logging_redirect_tqdm
 import xarray as xr
 
+from mlde_utils.torch import XRDataset
+
 
 def generate_np_samples(model, cond_batch):
     model.eval()
@@ -53,14 +55,19 @@ def np_samples_to_xr(samples, xr_eval_ds, target_transform):
     return pred_ds
 
 
-def sample(model, eval_dl, target_transform):
+def sample(model, xr_data_eval, batch_size, variables, target_transform):
     np_samples = []
     with logging_redirect_tqdm():
         with tqdm(
-            total=len(eval_dl.dataset), desc=f"Sampling", unit=" timesteps"
+            total=len(xr_data_eval["time"]), desc=f"Sampling", unit=" timesteps"
         ) as pbar:
             with torch.no_grad():
-                for batch_num, (cond_batch, _) in enumerate(eval_dl):
+                for i in range(0, xr_data_eval["time"].shape[0], batch_size):
+                    batch_times = xr_data_eval["time"][i : i + batch_size]
+                    batch_ds = xr_data_eval.sel(time=batch_times)
+
+                    cond_batch = XRDataset.to_tensor(batch_ds, variables)
+
                     batch_np_samples = generate_np_samples(model, cond_batch)
                     np_samples.append(batch_np_samples)
 
@@ -68,7 +75,6 @@ def sample(model, eval_dl, target_transform):
 
     # combine the samples along the time/batch axis
     np_samples = np.concatenate(np_samples, axis=0)
-    xr_data_eval = eval_dl.dataset.ds
 
     xr_samples = np_samples_to_xr(np_samples, xr_data_eval, target_transform)
 
